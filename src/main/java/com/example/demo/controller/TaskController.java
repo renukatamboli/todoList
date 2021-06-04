dd9a041576cf7da87dd59bf2652206e6e27ced84
Renuka Tamboli <renuka@interleap.co>
2021-06-04 17:57:12 +0530

    added get mapping

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -1,23 +1,36 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.Task;
 import com.example.demo.service.TaskService;
-
-
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
 	@Autowired
-    TaskService ts;
-	 @PostMapping("/")
-	    public void add(@RequestBody Task t) {
-	        ts.addTask(t);
-	    }
+	TaskService ts;
+
+	@PostMapping("/")
+	public void add(@RequestBody Task t) {
+		ts.addTask(t);
+	}
+
+	@GetMapping
+	public List<Task> listTasks() {
+		return ts.getAllTask();
+	}
+
+	@GetMapping("/{id}")
+	public Task get(@PathVariable("id") Integer id) {
+		return ts.getTask(id);
+	}
 }
--- src/main/java/com/example/demo/service/TaskService.java
+++ src/main/java/com/example/demo/service/TaskService.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,5 +20,12 @@ public class TaskService {
 	
 	public void addTask(Task t) {
         repo.save(t);
+    }
+	
+	public Task getTask(Integer id) {
+		return repo.getById(id);
+	}
+	public List<Task> getAllTask() {
+        return repo.findAll();
     }
 }
