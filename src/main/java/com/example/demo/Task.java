ccbb54d1b6353b8692781724880f2bc360e4deb9
Renuka Tamboli <renuka@interleap.co>
2021-06-04 12:55:22 +0530

    added fields in class Task

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -1,5 +1,32 @@
 package com.example.demo;
 
 public class Task {
-
+	private int id;
+    private String label;
+    private String description;
+    private String deadline;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLabel() {
+		return label;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
+	}
 }
