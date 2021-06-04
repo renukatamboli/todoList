699db02cb17cb708389bba2545980ccbee07057c
Renuka Tamboli <renuka@interleap.co>
2021-06-04 14:21:56 +0530

    created files

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -1,32 +1,37 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Task {
+	@Id
 	private int id;
-    private String label;
-    private String description;
-    private String deadline;
+	private String label;
+	private String description;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getLabel() {
 		return label;
 	}
+
 	public void setLabel(String label) {
 		this.label = label;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getDeadline() {
-		return deadline;
-	}
-	public void setDeadline(String deadline) {
-		this.deadline = deadline;
-	}
+
 }
